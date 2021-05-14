25f8b751999359a4558229b846a2c753985c56d4
tarynblakemiller <yourfriendtaryn@gmail.com>
2021-05-14 13:39:39 -0400

    added to seed file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello, the app is running!");
 });
 
 app.get("/comments", (req, res) => {
--- seed.js
+++ seed.js
@@ -17,5 +17,5 @@ db.connect((err) => {
 });
 
 db.query(
-  'INSERT INTO `commentdata` (content) VALUES ("blah"), ("yeah"), ("whoa")'
+  'INSERT INTO `commentdata` (content) VALUES ("That\'s crazy ngl"), ("Wow.. Great work  !!"), ("Yeah, thanks, no wonder why I can\'t ever get a good pair of Jordan\'s"), ("i cant imagine hm ppl are gonna try to take up botting after this video and lose $ lol"), ("Who else is thinking about buying sneakers now?"), ("I buy 99% of my clothing online thrifts.")'
 );
